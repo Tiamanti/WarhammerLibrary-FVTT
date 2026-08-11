702de4b8bf3d0d657ca4b79a31ff969499d54bb8
Russell <russellthurman94@gmail.com>
2026-08-10 22:36:25 -0500

    handle version numbers during build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,19 +1,39 @@
 /* eslint-disable no-undef */
-import foundryConfig from "./foundry-path.js";
+import foundryPath from "./foundry-path.mjs";
 import copy from 'rollup-plugin-copy';
 import postcss from "rollup-plugin-postcss";
+import simpleGit from 'simple-git';
+import yargs from 'yargs';
+import fs from "fs";
 
-let modulePath = foundryConfig();
+
+let args = yargs(process.argv.slice(2)).parse();
+
+let latest = args.configLatest;
+if (!latest)
+{
+    latest = await new Promise(resolve => {
+        simpleGit({baseDir: process.cwd()}).tags((err, tags) => resolve(tags.latest));
+    })
+}
+
+let manifest = JSON.parse(fs.readFileSync("./module.json"));
+let modulePath = foundryPath(manifest.id, manifest.compatibility.verified);
+
+console.log("Setting Version " + latest)
+console.log("Bundling to " + modulePath);
+
+
 export default {
 	input: ['src/warhammer-lib.js'],
 	output: {dir: modulePath},
 	plugins: [
 		copy({
 			targets : [
-				{src: "./module.json", dest : modulePath},
+				{src: "./module.json", dest : modulePath, transform: (contents) => contents.toString().replaceAll("@VERSION", latest)},
 				{src: "./static/*", dest : modulePath}
 			],
-            watch: process.env.NODE_ENV == "production" ? false : ["./static/**", "system.json", "template.json"]
+            watch: process.env.NODE_ENV == "production" ? false : ["./static/**", "module.json"]
 		}),
 		postcss({
 			extract: "warhammer.css",
